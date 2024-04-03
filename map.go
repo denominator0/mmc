2b0cbc28d4bce4508ce8ff12299683aaefbad34d
denominator0 <denominatorzero@outlook.com>
2024-04-04 01:24:37 +0800

    fix ref cycle

--- map.go
+++ map.go
@@ -1,16 +1,25 @@
 package mmc
 
-func Map[E, AfterE any, S ~[]E](slice S, m func(itera Itera[E]) AfterE) []AfterE {
-	result, _ := MapE(slice, func(itera Itera[E]) (AfterE, error) { return m(itera), nil })
+func Map[BEF, AFT any, S ~[]BEF](slice S, m func(itera Itera[BEF]) AFT) []AFT {
+	result, _ := MapE(slice, func(itera Itera[BEF]) (AFT, error) { return m(itera), nil })
 	return result
 }
 
-func MapE[E, AfterE any, S ~[]E](slice S, m func(itera Itera[E]) (AfterE, error)) ([]AfterE, error) {
-	return FoldE(slice, func(sum []AfterE, itera Itera[E]) ([]AfterE, error) {
-		piece, err := m(itera)
-		if err != nil {
-			return nil, err
-		}
-		return append(sum, piece), nil
-	})(make([]AfterE, 0, len(slice)))
+func MapE[BEF, AFT any, S ~[]BEF](slice S, m func(itera Itera[BEF]) (AFT, error)) ([]AFT, error) {
+	if len(slice) == 0 {
+		return []AFT{}, nil
+	}
+	head, err := m(Itera[BEF]{Cur: slice[0]})
+	if err != nil {
+		return nil, err
+	}
+	tail, err := MapE(slice[1:], func(itera Itera[BEF]) (AFT, error) {
+		itera.Pos += 1
+		return m(itera)
+	})
+	if err != nil {
+		return nil, err
+	}
+	result := append(make([]AFT, 0, 1+len(tail)), head)
+	return append(result, tail...), nil
 }
